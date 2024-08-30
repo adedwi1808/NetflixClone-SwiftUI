d1d54ff977be506666ce69276c0cc761fb57ff02
Ade Dwi Prayitno <adedwip1808@email.com>
2024-08-30 08:39:24 +0700

    Initial Commit

---
+++ NetflixClone-SwiftUI/NetflixClone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixClone_SwiftUIApp.swift
+//  NetflixClone-SwiftUI
+//
+//  Created by Ade Dwi Prayitno on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct NetflixClone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NetflixClone-SwiftUIUITests/NetflixClone_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NetflixClone_SwiftUIUITestsLaunchTests.swift
+//  NetflixClone-SwiftUIUITests
+//
+//  Created by Ade Dwi Prayitno on 30/08/24.
+//
+
+import XCTest
+
+final class NetflixClone_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
